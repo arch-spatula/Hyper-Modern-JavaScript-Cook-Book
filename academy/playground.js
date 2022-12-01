28deadf1dc1acfa62992d1c1df227223c15d4a21
arch-spatula <shkim516@naver.com>
2022-12-01 22:58:25 +0900

    학원 강의내용 추가

--- academy/playground.js
+++ academy/playground.js
@@ -1,10 +1,2 @@
-var obj = {
-  outer: function () {
-    console.log(this); // { outer: [Function: outer] }
-    var innerFunc = () => {
-      console.log(this); // { outer: [Function: outer] }
-    };
-    innerFunc();
-  },
-};
-obj.outer();
+// 프라미스 처리 결과가 담긴 배열을 기다립니다.
+let results = await Promise.all([fetch(url1), fetch(url2)]);
