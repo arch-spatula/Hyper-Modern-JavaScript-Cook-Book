9758e65de78472e7e79671093e257e0bb51ccb4d
arch-spatula <shkim516@naver.com>
2023-04-20 19:26:35 +0900

    feat: 히어로 배너 텍스트 컬러 수정 #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
 import styles from "./index.module.css";
 
 function HomepageHeader() {
@@ -12,8 +11,8 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary bg-img", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title text-color">{siteConfig.title}</h1>
+        <p className="hero__subtitle text-color">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -29,6 +28,7 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
